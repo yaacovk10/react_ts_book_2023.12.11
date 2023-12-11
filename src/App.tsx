5cd5a07caf00a020cfabd1f83ae9a139036402c3
yaacovk10 <yaacovana@gmail.com>
2023-12-11 14:28:07 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Book from './Book';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [myname, setmyname] = useState("")
+  const [myauthor, setmyauthor] = useState("")
+  const [mypublish, setmypublish] = useState(0)
+  const [book, setbook] = useState<Book[]>([]);
+  const addBook= ()=>{
+    const newBook : Book= {
+        name :myname,
+        author: myauthor,
+        plubish: mypublish
+
+        }
+    setbook([...book,newBook])
+  }
+
+    return (
+       <div className='App'>
+        Name<input onChange={(e)=>setmyname(e.target.value)}/>
+        Author<input onChange={(e)=>setmyauthor(e.target.value)}/>
+        Publish<input onChange={(e)=>setmypublish(Number(e.target.value))}/>
+        <button onClick={()=>addBook()}>Add</button>
+        {book.length}
+       </div>
   );
 }
 
---
+++ src/Book.ts
@@ -0,0 +1,5 @@
+export default interface Book{
+    name: string
+    author: string
+    plubish:number
+}
